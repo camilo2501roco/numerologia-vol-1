fb464c0ed53fe6386246f94d69f8f9a0ae6d99dd
camilo2501roco <aparendizcamilo@gmail.com>
2025-11-10 20:27:19 -0500

    .env creado

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
 
 const pool = mysql.createPool({
      host: 'localhost',
-  user: 'root',          
-  password: '12345678',        
-  database: 'numerologia',  
+  user: process.env.DB_USER,          
+  password: process.env.DB_PASSWORD,        
+  database: process.env.DB_NAME,  
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
